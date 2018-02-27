3bebaa3b04708e9b6f3eeb9625f4af1fd3c2bfca
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-02-28 00:39:04 +0300

    home activity için fragmentler olusturuldu : camera, home ve message fragmentleri, bir sonraki derste bunları view pager ile gösterticez

---
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Home/CameraFragment.kt
@@ -0,0 +1,20 @@
+package com.emrealtunbilek.instakotlinapp.Home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emrealtunbilek.instakotlinapp.R
+
+/**
+ * Created by Emre on 28.02.2018.
+ */
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var view=inflater?.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+}
