30aecb17677a7af0aacee153f46b22e79a39a899
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-03-11 22:45:16 +0300

    profil actvity genel hatlarıyla hazır, toolbar ve bottom navigation view tamam

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileActivity.kt
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.emrealtunbilek.instakotlinapp.R
 import com.emrealtunbilek.instakotlinapp.utils.BottomnavigationViewHelper
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_profile.*
+
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -13,9 +14,9 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_profile)
 
-        setupNavigationView()
+       setupNavigationView()
     }
 
     fun setupNavigationView(){
