3911ef1633c0ff706fc6c945311d4dce635dc910
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-03-11 23:30:45 +0300

    profil settings activity olusturuldu

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileSettingsActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileSettingsActivity.kt
@@ -1,31 +1,29 @@
 package com.emrealtunbilek.instakotlinapp.Profile
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.emrealtunbilek.instakotlinapp.R
 import com.emrealtunbilek.instakotlinapp.utils.BottomnavigationViewHelper
-import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_profile_settings.*
 
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileSettingsActivity : AppCompatActivity() {
 
     private val ACTIVITY_NO=4
     private val TAG="ProfileActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_profile_settings)
 
-        setupToolbar()
         setupNavigationView()
+        setupToolbar()
     }
 
     private fun setupToolbar() {
-       imgProfileSettings.setOnClickListener {
-           var intent=Intent(this,ProfileSettingsActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-           startActivity(intent)
-       }
+        imgBack.setOnClickListener {
+           onBackPressed()
+        }
     }
 
     fun setupNavigationView(){
