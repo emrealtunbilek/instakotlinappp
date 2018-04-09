e57445175daacb09e7f261e9220be296d199b21c
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-04-09 23:56:06 +0300

    universal image loader eklendi ayarları yapıldı ve de edit fragmentteki resim internetten geliyor

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileEditFragment.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Profile/ProfileEditFragment.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.emrealtunbilek.instakotlinapp.R
+import com.emrealtunbilek.instakotlinapp.utils.UniversalImageLoader
+import com.nostra13.universalimageloader.core.ImageLoader
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_profile_edit.*
 import kotlinx.android.synthetic.main.fragment_profile_edit.view.*
 
@@ -17,11 +20,18 @@ import kotlinx.android.synthetic.main.fragment_profile_edit.view.*
  */
 class ProfileEditFragment : Fragment() {
 
+    lateinit var circleProfileImageFragment:CircleImageView
+
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val view=inflater!!.inflate(R.layout.fragment_profile_edit, container, false)
+
+        circleProfileImageFragment=view.findViewById(R.id.circleProfileImage)
+        initImageLoader()
+        setupProfilePicture()
+
 
         view.imgClose.setOnClickListener {
 
@@ -31,6 +41,22 @@ class ProfileEditFragment : Fragment() {
 
 
         return view
+    }
+
+    private fun initImageLoader(){
+
+        var universalImageLoader=UniversalImageLoader(activity)
+        ImageLoader.getInstance().init(universalImageLoader.config)
+
+    }
+
+    private fun setupProfilePicture() {
+
+        //https://orig00.deviantart.net/67cd/f/2012/309/8/c/android_icon_by_gabrydesign-d4m7he9.png
+        var imgURL= "orig00.deviantart.net/67cd/f/2012/309/8/c/android_icon_by_gabrydesign-d4m7he9.png"
+        UniversalImageLoader.setImage(imgURL,circleProfileImageFragment,null,"https://")
+
+
     }
 
 }
---
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/utils/UniversalImageLoader.kt
@@ -0,0 +1,97 @@
+package com.emrealtunbilek.instakotlinapp.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.emrealtunbilek.instakotlinapp.R
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache
+import com.nostra13.universalimageloader.core.DisplayImageOptions
+import com.nostra13.universalimageloader.core.ImageLoader
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration
+import com.nostra13.universalimageloader.core.assist.FailReason
+import com.nostra13.universalimageloader.core.assist.ImageScaleType
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener
+
+/**
+ * Created by Emre on 9.04.2018.
+ */
+class UniversalImageLoader(val mContext:Context) {
+
+    val config:ImageLoaderConfiguration
+        get(){
+
+          val defaultOptions = DisplayImageOptions.Builder()
+                  .showImageOnLoading(defaultImage)
+                  .showImageForEmptyUri(defaultImage)
+                  .showImageOnFail(defaultImage)
+                  .cacheOnDisk(true).cacheInMemory(true)
+                  .cacheOnDisk(true).resetViewBeforeLoading(true)
+                  .imageScaleType(ImageScaleType.EXACTLY)
+                  .displayer(FadeInBitmapDisplayer(400)).build()
+
+            return ImageLoaderConfiguration.Builder(mContext)
+                    .defaultDisplayImageOptions(defaultOptions)
+                    .memoryCache(WeakMemoryCache())
+                    .diskCacheSize(100*1024*1024).build()
+
+        }
+
+    companion object {
+
+        private val defaultImage = R.drawable.ic_profile
+
+        fun setImage(imgURL:String, imageView:ImageView, mProgressBar:ProgressBar?, ilkKisim:String){
+
+            //imgUrl:facebook.com/images/logo.jpeg
+            //ilkKisim:http://
+
+            val imageLoader=ImageLoader.getInstance()
+            imageLoader.displayImage(ilkKisim+imgURL,imageView, object : ImageLoadingListener{
+                override fun onLoadingComplete(imageUri: String?, view: View?, loadedImage: Bitmap?) {
+
+                    if(mProgressBar!=null){
+
+                        mProgressBar.visibility=View.GONE
+
+                    }
+
+                }
+
+                override fun onLoadingStarted(imageUri: String?, view: View?) {
+                    if(mProgressBar!=null){
+
+                        mProgressBar.visibility=View.VISIBLE
+
+                    }
+                }
+
+                override fun onLoadingCancelled(imageUri: String?, view: View?) {
+                    if(mProgressBar!=null){
+
+                        mProgressBar.visibility=View.GONE
+
+                    }
+                }
+
+                override fun onLoadingFailed(imageUri: String?, view: View?, failReason: FailReason?) {
+                    if(mProgressBar!=null){
+
+                        mProgressBar.visibility=View.GONE
+
+                    }
+                }
+
+
+            })
+
+
+
+        }
+
+    }
+
+
+}
