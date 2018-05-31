b313b5997d8f77ba28f1a5c7891c103673bb1b76
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-06-01 02:53:59 +0300

    fotoğraf çekip dosyaya dönüştürdük,,sonrasında bu olusan yeni dosya sıkıstırıldı ve firebase stroagea gönderildi

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Share/ShareCameraFragment.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Share/ShareCameraFragment.kt
@@ -2,6 +2,7 @@ package com.emrealtunbilek.instakotlinapp.Share
 
 
 import android.os.Bundle
+import android.os.Environment
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,8 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.emrealtunbilek.instakotlinapp.R
+import com.emrealtunbilek.instakotlinapp.utils.EventbusDataEvents
+import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.CameraView
+import com.otaliastudios.cameraview.Gesture
+import com.otaliastudios.cameraview.GestureAction
+import kotlinx.android.synthetic.main.activity_share.*
+import kotlinx.android.synthetic.main.fragment_share_camera.*
 import kotlinx.android.synthetic.main.fragment_share_camera.view.*
+import org.greenrobot.eventbus.EventBus
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 
 class ShareCameraFragment : Fragment() {
@@ -24,6 +35,42 @@ class ShareCameraFragment : Fragment() {
         var view=inflater.inflate(R.layout.fragment_share_camera, container, false)
 
         cameraView=view.videoView
+        cameraView.mapGesture(Gesture.PINCH, GestureAction.ZOOM)
+        cameraView.mapGesture(Gesture.TAP, GestureAction.FOCUS_WITH_MARKER)
+
+        view.imgFotoCek.setOnClickListener {
+            cameraView.capturePicture()
+        }
+
+        cameraView.addCameraListener(object : CameraListener(){
+
+            override fun onPictureTaken(jpeg: ByteArray?) {
+                super.onPictureTaken(jpeg)
+
+                var cekilenFotoAdi=System.currentTimeMillis()
+                var cekilenFoto=File(Environment.getExternalStorageDirectory().absolutePath+"/DCIM/TestKlasor/"+cekilenFotoAdi+".jpg")
+
+                var dosyaOlustur=FileOutputStream(cekilenFoto)
+                dosyaOlustur.write(jpeg)
+                dosyaOlustur.close()
+
+                activity!!.anaLayout.visibility= View.GONE
+                activity!!.fragmentContainerLayout.visibility=View.VISIBLE
+                var transaction=activity!!.supportFragmentManager.beginTransaction()
+
+                EventBus.getDefault().postSticky(EventbusDataEvents.PaylasilacakResmiGonder(cekilenFoto.absolutePath.toString(),true))
+                transaction.replace(R.id.fragmentContainerLayout,ShareNextFragment())
+                transaction.addToBackStack("shareNextFragmentEklendi")
+                transaction.commit()
+
+
+                Log.e("HATA2","cekilen resim buraya kaydedildi :"+cekilenFoto.absolutePath.toString())
+
+
+
+            }
+
+        })
 
         return view
     }
