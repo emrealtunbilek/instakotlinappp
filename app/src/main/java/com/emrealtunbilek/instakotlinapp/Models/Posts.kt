f09d3e80887066f91f98182fc18c9b79e4794af6
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-06-05 03:35:28 +0300

    home fragmentta takipçi ve kendi paylaşımlarımızı göstermek için recyclerview kullanacağız, bunun için model sınıf ve  tek bir öğeyi ele alan layoutu hazırladık

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Models/Posts.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Models/Posts.kt
@@ -7,12 +7,12 @@ class Posts {
 
     var user_id:String? = null
     var post_id:String? = null
-    var yuklenme_tarih:String? = null
+    var yuklenme_tarih:Long? = null
     var aciklama:String? = null
     var file_url:String? = null
 
     constructor(){}
-    constructor(user_id: String?, post_id: String?, yuklenme_tarih: String?, aciklama: String?, photo_url: String?) {
+    constructor(user_id: String?, post_id: String?, yuklenme_tarih: Long?, aciklama: String?, photo_url: String?) {
         this.user_id = user_id
         this.post_id = post_id
         this.yuklenme_tarih = yuklenme_tarih
---
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Models/UserPosts.kt
@@ -0,0 +1,15 @@
+package com.emrealtunbilek.instakotlinapp.Models
+
+/**
+ * Created by Emre on 5.06.2018.
+ */
+class UserPosts {
+
+    var userID:String?=null
+    var userName:String?=null
+    var userPhotoURL:String?=null
+    var postID:String?=null
+    var postAciklama:String?=null
+    var postURL:String?=null
+
+}
