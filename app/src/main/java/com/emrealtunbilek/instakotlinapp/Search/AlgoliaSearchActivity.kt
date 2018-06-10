97a5c54820b7c0dea4bbf8649351262b30aca904
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-06-10 21:34:46 +0300

    algolia için yeni bir activity oluşturuldu, kütüphaneler güncellendi, library çakışmaları giderildi

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Search/AlgoliaSearchActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Search/AlgoliaSearchActivity.kt
@@ -1,30 +1,21 @@
 package com.emrealtunbilek.instakotlinapp.Search
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.emrealtunbilek.instakotlinapp.R
 import com.emrealtunbilek.instakotlinapp.utils.BottomnavigationViewHelper
-import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.activity_algolia_search.*
 
-class SearchActivity : AppCompatActivity() {
+class AlgoliaSearchActivity : AppCompatActivity() {
 
     private val ACTIVITY_NO=1
-    private val TAG="SearchActivity"
+    private val TAG="AlgoliaSearchActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_algolia_search)
 
         setupNavigationView()
-
-        searchview.setOnClickListener {
-
-            var intent=Intent(this,AlgoliaSearchActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
-
-        }
     }
 
     fun setupNavigationView(){
