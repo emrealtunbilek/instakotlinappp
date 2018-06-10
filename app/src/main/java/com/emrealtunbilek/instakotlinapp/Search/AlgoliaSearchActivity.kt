94620e60bd21dd31289330f142b202edee0aa495
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-06-10 22:57:25 +0300

    algolia çalısacak şekilde ayarlandı,, google clouddaki server sürekli olarak çalısmaya basladı.. eskiden terminali kapattıgımızda sona eriyordu artık arka planda sürekli çalısıyor

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/Search/AlgoliaSearchActivity.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/Search/AlgoliaSearchActivity.kt
@@ -5,17 +5,37 @@ import android.os.Bundle
 import com.emrealtunbilek.instakotlinapp.R
 import com.emrealtunbilek.instakotlinapp.utils.BottomnavigationViewHelper
 import kotlinx.android.synthetic.main.activity_algolia_search.*
+import com.algolia.instantsearch.helpers.InstantSearch
+import com.algolia.instantsearch.helpers.Searcher
+
+
 
 class AlgoliaSearchActivity : AppCompatActivity() {
 
     private val ACTIVITY_NO=1
     private val TAG="AlgoliaSearchActivity"
+    private val ALGOLIA_APP_ID = "60M8DCAPLT"
+    private val ALGOLIA_SEARCH_API_KEY = "3c832a8c9c865059604c76bc54f44644"
+    private val ALGOLIA_INDEX_NAME = "KotlinInstagram"
+    lateinit var searcher:Searcher
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_algolia_search)
 
         setupNavigationView()
+        setupAlgoliaSearch()
+
+    }
+
+    private fun setupAlgoliaSearch() {
+        searcher = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY, ALGOLIA_INDEX_NAME)
+        val helper = InstantSearch(this, searcher)
+        helper.search()
+
+        imgBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 
     fun setupNavigationView(){
@@ -25,5 +45,10 @@ class AlgoliaSearchActivity : AppCompatActivity() {
         var menu=bottomNavigationView.menu
         var menuItem=menu.getItem(ACTIVITY_NO)
         menuItem.setChecked(true)
+    }
+
+    override fun onDestroy() {
+        searcher.destroy()
+        super.onDestroy()
     }
 }
