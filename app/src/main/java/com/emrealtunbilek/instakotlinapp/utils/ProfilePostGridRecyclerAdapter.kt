5c09a9fd294809af54cd0784bac79a4064ce0b8d
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-06-12 21:16:37 +0300

    video dosyalarını firebaseden cekerken thumbnail olmadıgından gösterilmiyordu, onu düzelttik

--- app/src/main/java/com/emrealtunbilek/instakotlinapp/utils/ProfilePostGridRecyclerAdapter.kt
+++ app/src/main/java/com/emrealtunbilek/instakotlinapp/utils/ProfilePostGridRecyclerAdapter.kt
@@ -5,13 +5,18 @@ import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.emrealtunbilek.instakotlinapp.Models.UserPosts
 import com.emrealtunbilek.instakotlinapp.R
-import kotlinx.android.synthetic.main.tek_sutun_grid_resim.view.*
+import kotlinx.android.synthetic.main.tek_sutun_grid_resim_profil.view.*
 import org.greenrobot.eventbus.EventBus
+import android.os.Build
+import android.graphics.Bitmap
+
+
 
 /**
  * Created by Emre on 3.06.2018.
@@ -33,7 +38,7 @@ class ProfilePostGridRecyclerAdapter(var kullaniciPostlari:ArrayList<UserPosts>,
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
-        var tekSutunDosya=inflater.inflate(R.layout.tek_sutun_grid_resim, parent, false)
+        var tekSutunDosya=inflater.inflate(R.layout.tek_sutun_grid_resim_profil, parent, false)
 
         return MyViewHolder(tekSutunDosya)
 
@@ -44,65 +49,62 @@ class ProfilePostGridRecyclerAdapter(var kullaniciPostlari:ArrayList<UserPosts>,
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
         var dosyaYolu= kullaniciPostlari.get(position).postURL
-        //var dosyaTuru=dosyaYolu.substring(dosyaYolu.lastIndexOf("."))
+        var noktaninGectigiIndex=dosyaYolu!!.lastIndexOf(".")
+        var dosyaTuru=dosyaYolu!!.substring(noktaninGectigiIndex, noktaninGectigiIndex+4)
 
-        holder.videoSure.visibility=View.GONE
-        UniversalImageLoader.setImage(dosyaYolu!!, holder.dosyaResim, holder.dosyaProgressBar,"")
 
-        /*
         if(dosyaTuru.equals(".mp4")){
-
-            holder.videoSure.visibility=View.VISIBLE
-            var retriver= MediaMetadataRetriever()
-            retriver.setDataSource(myContext, Uri.parse("file://"+dosyaYolu))
-
-            var videoSuresi=retriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
-            var videoSuresiLong=videoSuresi.toLong()
-
-
-            holder.videoSure.setText(convertDuration(videoSuresiLong))
-            UniversalImageLoader.setImage(dosyaYolu, holder.dosyaResim, holder.dosyaProgressBar,"file:/")
-
+            Log.e("DOSYA TURU","DOSYA TURU"+ dosyaTuru)
+            holder.videoIcon.visibility=View.VISIBLE
+            var thumnnailFoto=videodanThumbnailOlustur(dosyaYolu)
+            holder.dosyaResim.setImageBitmap(thumnnailFoto)
+            holder.dosyaProgressBar.visibility=View.GONE
         }else {
-
-
-
+            Log.e("DOSYA TURU","DOSYA TURU"+ dosyaTuru)
+            holder.videoIcon.visibility=View.GONE
+            holder.dosyaProgressBar.visibility=View.VISIBLE
+            UniversalImageLoader.setImage(dosyaYolu!!, holder.dosyaResim, holder.dosyaProgressBar,"")
         }
 
-        holder.tekSutunDosya.setOnClickListener {
-            EventBus.getDefault().post(EventbusDataEvents.GalerySecilenDosyaYolunuGonder(dosyaYolu))
-        }*/
+    }
 
+    @Throws(Throwable::class)
+    fun videodanThumbnailOlustur(videoPath: String): Bitmap? {
+        var bitmap: Bitmap? = null
+        var mediaMetadataRetriever: MediaMetadataRetriever? = null
+        try {
+            mediaMetadataRetriever = MediaMetadataRetriever()
+            if (Build.VERSION.SDK_INT >= 14)
+                mediaMetadataRetriever.setDataSource(videoPath, HashMap())
+            else
+                mediaMetadataRetriever.setDataSource(videoPath)
+
+            bitmap = mediaMetadataRetriever.getFrameAtTime(1, MediaMetadataRetriever.OPTION_CLOSEST)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw Throwable(
+                    "Exception in retriveVideoFrameFromVideo(String videoPath)" + e.message)
+
+        } finally {
+            if (mediaMetadataRetriever != null) {
+                mediaMetadataRetriever.release()
+            }
+        }
+        return bitmap
     }
 
 
     class MyViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
 
         var tekSutunDosya=itemView as ConstraintLayout
-
+        var videoIcon=tekSutunDosya.imgVideoIcon
         var dosyaResim=tekSutunDosya.imgTekSutunImage
-        var videoSure=tekSutunDosya.tvSure
         var dosyaProgressBar=tekSutunDosya.progressBar
 
 
 
     }
 
-    fun convertDuration(duration: Long): String {
-        val second = duration / 1000 % 60
-        val minute = duration / (1000 * 60) % 60
-        val hour = duration / (1000 * 60 * 60) % 24
-
-        var time=""
-        if(hour>0){
-            time = String.format("%02d:%02d:%02d", hour, minute, second)
-        }else {
-            time = String.format("%02d:%02d", minute, second)
-        }
-
-        return time
-
-    }
 
 
 }
